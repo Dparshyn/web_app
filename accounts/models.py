38f377e52f10cda890633dbc8f8b87a5a193479f
Dmitriy Parshin <sdmitriy1984@gmail.com>
2020-09-11 19:23:12 +0300

    Models creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
     login = models.CharField(max_length=255, unique=True)
@@ -12,3 +11,19 @@ class Profile(models.Model):
         return f'nickname - {self.nickname}, id - {self.id}, password - {self.password}'
 
 
+class Publication(models.Model):
+    publication_date = models.DateTimeField('Date of comment ', auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', null=False, blank=True)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.author, self.publication_date)
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    text = models.CharField(Publication, max_length=2000)
+
+    def __str__(self):
+        return self.text
